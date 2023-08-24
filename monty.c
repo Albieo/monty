c979514251716082f4d4054f2b514352f6d29101
Albert Mpepo <albertmpepo@gmail.com>
2023-08-25 00:31:34 +0200

    Program now ignores whitespaces

--- monty.c
+++ monty.c
@@ -2,82 +2,83 @@
 
 int main(int argc, char *argv[])
 {
-	stack_t *head = NULL;
-	FILE *file;
-	char line[50];
-	char *function;
-	char *arg_str;
-	int i, argument = 0;
-	struct
-	{
-		char *name;
-		int index;
-	}
+    stack_t *head = NULL;
+    FILE *file;
+    char line[50];
+    char *token;
+    char *arg_str;
+    int i, argument = 0;
+    struct
+    {
+        char *name;
+        int index;
+    }
+    functions[] = {
+        {"push", 0},
+        {"pall", 1},
+        {"pint", 2},
+        {"pop", 3},
+        {"swap", 4},
+        {"add", 5},
+        {"nop", 6},
+        {NULL, -1}
+    };
+    instruction_t instructions[] = {
+        {"push", push},
+        {"pall", pall},
+        {"pint", pint},
+        {"pop", pop},
+        {"swap", swap},
+        {"add", add},
+        {"nop", nop},
+        {NULL, NULL}
+    };
 
-	functions[] = {
-		{"push", 0},
-		{"pall", 1},
-		{"pint", 2},
-		{"pop", 3},
-		{"swap", 4},
-		{"add", 5},
-		{"nop", 6},
-		{NULL, -1}
-	};
-	instruction_t instructions[] = {
-		{"push", push},
-		{"pall", pall},
-		{"pint", pint},
-		{"pop", pop},
-		{"swap", swap},
-		{"add", add},
-		{"nop", nop},
-		{NULL, NULL}
-	};
+    if (argc != 2)
+    {
+        printf("Usage: ./monty filename.m\n");
+        return (1);
+    }
 
-	if (argc != 2)
-	{
-		printf("Usage: ./monty filename.m\n");
-		return (1);
-	}
+    file = fopen(argv[1], "r");
 
-	file = fopen(argv[1], "r");
+    if (file == NULL)
+    {
+        printf("Failed to open the file.\n");
+        return (1);
+    }
 
-	if (file == NULL)
-	{
-		printf("Failed to open the file.\n");
-		return (1);
-	}
+    while (fgets(line, sizeof(line), file) != NULL)
+    {
+        token = strtok(line, " \n\t");
+        while (token != NULL)
+        {
+            if (strcmp(token, "push") == 0)
+            {
+                arg_str = strtok(NULL, " \n\t");
+                if (arg_str == NULL)
+                {
+                    printf("L<line_number>: usage: push integer\n");
+                    exit(EXIT_FAILURE);
+                }
+                argument = atoi(arg_str);
+            }
+            else
+                argument = 0;
 
-	while (fgets(line, sizeof(line), file) != NULL)
-	{
-		function = strtok(line, " \n");
-		arg_str = strtok(NULL, " \n");
+            for (i = 0; functions[i].name != NULL; i++)
+            {
+                if (strcmp(token, functions[i].name) == 0)
+                {
+                    instructions[functions[i].index].f(&head, argument);
+                    break;
+                }
+            }
+            token = strtok(NULL, " \n\t");
+        }
+    }
 
-		if (strcmp(function, "push") == 0 && arg_str == NULL)
-		{
-			printf("L<line_number>: usage: push integer\n");
-			exit(EXIT_FAILURE);
-		}
+    fclose(file);
 
-		if (arg_str != NULL)
-			argument = atoi(arg_str);
-
-
-		if (function != NULL)
-		{
-			for (i = 0; functions[i].name != NULL; i++)
-			{
-				if (strcmp(function, functions[i].name) == 0)
-				{
-					instructions[functions[i].index].f(&head, argument);
-					break;
-				}
-			}
-		}
-	}
-
-	fclose(file);
-
-	return (0);
+    return (0);
 }
