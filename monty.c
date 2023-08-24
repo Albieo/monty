959c0eb0abd8cf91a413d8e288a69ebb59c1c77e
Albert Mpepo <albertmpepo@gmail.com>
2023-08-25 00:58:15 +0200

    split function is main file

--- monty.c
+++ monty.c
@@ -1,10 +1,7 @@
 #include "monty.h"
 
-int main(int argc, char *argv[])
+void processLine(char *line, stack_t **head)
 {
-    stack_t *head = NULL;
-    FILE *file;
-    char line[50];
     char *token;
     char *arg_str;
     int i, argument = 0;
@@ -34,51 +31,63 @@ int main(int argc, char *argv[])
         {NULL, NULL}
     };
 
+    token = strtok(line, " \n\t");
+    while (token != NULL)
+    {
+        if (strcmp(token, "push") == 0)
+        {
+            arg_str = strtok(NULL, " \n\t");
+            if (arg_str == NULL)
+            {
+                printf("L<line_number>: usage: push integer\n");
+                exit(EXIT_FAILURE);
+            }
+            argument = atoi(arg_str);
+        }
+        else
+            argument = 0;
+
+        for (i = 0; functions[i].name != NULL; i++)
+        {
+            if (strcmp(token, functions[i].name) == 0)
+            {
+                instructions[functions[i].index].f(head, argument);
+                break;
+            }
+        }
+        token = strtok(NULL, " \n\t");
+    }
+}
+
+void processFile(const char *filename)
+{
+    stack_t *head = NULL;
+    FILE *file;
+    char line[50];
+
+    file = fopen(filename, "r");
+
+    if (file == NULL)
+    {
+        printf("Failed to open the file.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    while (fgets(line, sizeof(line), file) != NULL)
+        processLine(line, &head);
+
+    fclose(file);
+}
+
+int main(int argc, char *argv[])
+{
     if (argc != 2)
     {
         printf("Usage: ./monty filename.m\n");
         return (1);
     }
 
-    file = fopen(argv[1], "r");
-
-    if (file == NULL)
-    {
-        printf("Failed to open the file.\n");
-        return (1);
-    }
-
-    while (fgets(line, sizeof(line), file) != NULL)
-    {
-        token = strtok(line, " \n\t");
-        while (token != NULL)
-        {
-            if (strcmp(token, "push") == 0)
-            {
-                arg_str = strtok(NULL, " \n\t");
-                if (arg_str == NULL)
-                {
-                    printf("L<line_number>: usage: push integer\n");
-                    exit(EXIT_FAILURE);
-                }
-                argument = atoi(arg_str);
-            }
-            else
-                argument = 0;
-
-            for (i = 0; functions[i].name != NULL; i++)
-            {
-                if (strcmp(token, functions[i].name) == 0)
-                {
-                    instructions[functions[i].index].f(&head, argument);
-                    break;
-                }
-            }
-            token = strtok(NULL, " \n\t");
-        }
-    }
-
-    fclose(file);
+    processFile(argv[1]);
 
     return (0);
 }
